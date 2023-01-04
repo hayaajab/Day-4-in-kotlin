f108e1778af2fa1a8f0e3f899a149ce362a69c46
Haya <52868771+hayaajab@users.noreply.github.com>
2023-01-04 08:48:28 +0300

    Update Arry_Assignment.kt

--- Arry_Assignment.kt
+++ Arry_Assignment.kt
@@ -1,17 +1,6 @@
 package com.example.day3
 
 fun main() {
-   /* val averag_array = arrayListOf<Double>(1.0,2.2,3.6,4.4,5.0)
-
-    for (index:Double in averag_array) {
-        var sum:Double = 0.0
-        sum += index
-        var a = averag_array.size
-        println( sum /a.toDouble())
-
-
-
-    }*/
 
     val numArray = doubleArrayOf(6.0,5.8,2.4,4.6,7.8)
     var sum = 0.0
@@ -20,10 +9,4 @@ fun main() {
     }
     val average = sum / numArray.size
     println("the average is : $average")
-
-
-
-
-
-
 }
